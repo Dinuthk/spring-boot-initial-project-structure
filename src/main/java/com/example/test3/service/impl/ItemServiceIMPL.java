78ed659cf7cfe5bb189bfa2d2fccad170ce07a8c
Dinuthk <141445703+Dinuthk@users.noreply.github.com>
2024-10-19 22:26:17 +0530

    done model mapper

--- src/main/java/com/example/test3/service/impl/ItemServiceIMPL.java
+++ src/main/java/com/example/test3/service/impl/ItemServiceIMPL.java
@@ -8,6 +8,8 @@ import com.example.test3.entity.enums.MeasuringUnitType;
 import com.example.test3.repo.CustomerRepo;
 import com.example.test3.repo.ItemRepo;
 import com.example.test3.service.ItemService;
+import com.sun.jdi.request.DuplicateRequestException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,18 +18,28 @@ public class ItemServiceIMPL implements ItemService {
 
     @Autowired
     private ItemRepo itemRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
     @Override
     public String saveItem(ItemSaveRequestDTO itemSaveRequestDTO) {
-        Item item=new Item(
-                1,
-                itemSaveRequestDTO.getItemName(),
-                itemSaveRequestDTO.getMeasuringUnitType(),
-                itemSaveRequestDTO.getBalanceQty(),
-                itemSaveRequestDTO.getSupplierPrice(),
-                itemSaveRequestDTO.getSellingPrice(),
-                true
-        );
-        itemRepo.save(item);
-        return itemSaveRequestDTO.getItemName();
+//        Item item=new Item(
+//                1,
+//                itemSaveRequestDTO.getItemName(),
+//                itemSaveRequestDTO.getMeasuringUnitType(),
+//                itemSaveRequestDTO.getBalanceQty(),
+//                itemSaveRequestDTO.getSupplierPrice(),
+//                itemSaveRequestDTO.getSellingPrice(),
+//                true
+//        );
+//        itemRepo.save(item);
+//        return itemSaveRequestDTO.getItemName(); meka parana krmaya alut eka use krnw modelemap mgin
+        Item item = modelMapper.map(itemSaveRequestDTO,Item.class);
+        if (!itemRepo.existsById(item.getItemID())){
+            itemRepo.save(item);
+            return item.getItemName()+"Saved sucussefully";
+        }else {
+            throw new DuplicateRequestException("Already added");
+        }
     }
 }
